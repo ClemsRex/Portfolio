594ad0a27d06b97b55a8c0eeb39ed39ea99e3d5d
ClemsRex <clementchukwu351@gmail.com>
2024-06-18 22:47:15 +0100

    Still styling the nav bar section

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menulinks")
+    const icon = document.querySelector(".food-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
