7914116fbab9c4c9b6b4b064b1b789f39ff563f0
ClemsRex <clementchukwu351@gmail.com>
2024-06-25 19:48:45 +0100

    modified the site

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menulinks")
-    const icon = document.querySelector(".food-icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
